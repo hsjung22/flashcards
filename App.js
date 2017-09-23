197a5925c92350f50a1c8db0a80c085335d6b6ca
hsjung22 <hsjung22@gmail.com>
2017-09-23 15:28:15 -0700

    feat: add tabs between 'Decks' and 'New Deck'

--- App.js
+++ App.js
@@ -1,14 +1,68 @@
-import React from 'react';
-import { View, Text, StatusBar } from 'react-native';
-import { pink } from './utils/colors';
-import DefaultStatusBar from './components/DefaultStatusBar';
+import React, { Component } from 'react'
+import { View, Platform, StatusBar } from 'react-native'
+import { FontAwesome, Ionicons } from '@expo/vector-icons'
+import { TabNavigator, StackNavigator } from 'react-navigation'
+import { pink, purple, white } from './utils/colors'
+import DefaultStatusBar from './components/DefaultStatusBar'
+import Decks from './components/Decks'
+import NewDeck from './components/NewDeck'
 
-export default class App extends React.Component {
+const Tabs = TabNavigator({
+  Decks:{
+    screen: Decks,
+    navigationOptions: {
+      tabBarLabel: 'Decks',
+      tabBarIcon: ({ tintColor }) => <Ionicons name='ios-bookmarks' size={30} color={tintColor} />
+    }
+  },
+  NewDeck:{
+    screen: NewDeck,
+    navigationOptions: {
+      tabBarLabel: 'New Deck',
+      tabBarIcon: ({ tintColor }) => <FontAwesome name='plus-square' size={30} color={tintColor} />
+    }
+  },
+}, {
+  navigationOptions: {
+    header: null
+  },
+  tabBarOptions: {
+    activeTintColor: Platform.OS === 'ios' ? purple : white,
+    style: {
+      height: 56,
+      backgroundColor: Platform.OS === 'ios' ? white : purple,
+      shadowColor: 'rgba(0,0,0,0.24)',
+      shadowOffset: {
+        width: 0,
+        height: 3,
+      },
+      shadowRadius: 6,
+      shadowOpacity: 1,
+    }
+  }
+})
+
+const MainNavigator = StackNavigator({
+  Home: {
+    screen: Tabs
+  },
+  // EntryDetail: {
+  //   screen: EntryDetail,
+  //   navigationOptions: {
+  //     headerTintColor: white,
+  //     headerStyle: {
+  //       backgroundColor: purple
+  //     }
+  //   }
+  // }
+})
+
+export default class App extends Component {
   render() {
     return (
-      <View>
+      <View style={{ flex: 1 }}>
         <DefaultStatusBar backgroundColor={pink} barStyle='light-content' />
-        <Text>Hello</Text>
+        <MainNavigator />
       </View>
     );
   }
